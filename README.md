4f653535d57ea0d590b90a5c83fe8d83dcdf79a4
Sachin Paudel <sachinpaudel32@gmail.com>
2026-06-30 20:09:34 +0545

    new commitment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # to-do-list
+
+## lets make a todo list
--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function addTodo(){
     const value = inputElement.value;
     console.log(value);
 }
+
+// -------------------------------------------------------------------------------------------------
+
+//     let ctr = 0;
+// function callback(){
+// const el = document.querySelector("h1");
+// el.innerHTML = ctr;
+//     console.log(ctr);
+//     // ctr = ctr + 1;
+// }
+
+// setInterval(callback, 1000);
+
+// ----------------------------------------------------------------------------------------------
+
+
+
+
