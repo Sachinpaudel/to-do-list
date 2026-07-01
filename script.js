4629356ad204f0a32cfd6f8d00e3408d54f56581
Sachin Paudel <sachinpaudel32@gmail.com>
2026-07-01 08:21:25 +0545

    again commitment

--- script.js
+++ script.js
@@ -18,6 +18,43 @@
 
 // ----------------------------------------------------------------------------------------------
 
+    function addTodo() {
+    const inputEl = document.querySelector("input");
+    const value = inputEl.value;
+
+    const newDivEl = document.createElement("div");
+    newDivEl.textContent = value;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.onclick = function () {
+        newDivEl.remove();
+    };
+
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+
+    editBtn.onclick = function () {
+        const newValue = prompt("Enter new value:", newDivEl.firstChild.textContent);
+
+        if (newValue !== null) {
+            newDivEl.firstChild.textContent = newValue;
+        }
+        else{
+            alert("Edit cancelled.");
+        }
+    };
+
+    newDivEl.appendChild(deleteBtn);
+    newDivEl.appendChild(editBtn);
+
+    document.body.appendChild(newDivEl);
+
+    inputEl.value = "";
+
+   
+}
 
 
 
